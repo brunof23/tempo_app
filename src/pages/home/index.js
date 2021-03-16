6a3e3c6196f7f5b2c1f1bec8a5431d3a3c8d8f2f
brunof23 <brunoferreira.tecnico@gmail.com>
2021-03-16 14:46:38 -0300

    add menu button

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
 
 export default function Home(){
     return(
-        <View>
+        <SafeAreaView style={styles.container}>
             <Text>Página Home</Text>
-        </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#e8f0ff',
+        paddingTop: '5%',
+        
+
+
+    }
+
+});
