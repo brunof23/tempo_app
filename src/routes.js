e384c0b6f7ee529f6f4efea5fef3aa25e93e87e0
brunof23 <brunoferreira.tecnico@gmail.com>
2021-03-15 22:32:13 -0300

    add search page

--- src/routes.js
+++ src/routes.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import Home from './pages/Home';
+import Search from './pages/Search';
+
+const Drawer = createDrawerNavigator();
+
+function Routes(){
+    return(
+        <Drawer.Navigator>
+            <Drawer.Screen
+                nome='Home'
+                component={Home}
+
+            />
+        </Drawer.Navigator>
+    )
+}
